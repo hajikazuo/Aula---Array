08a1535995d914794e080ea06f80a1c0f7532486
nilton kazuo <hajikazuo@outlook.com>
2022-05-25 21:44:32 -0300

    Adicionar arquivos de projeto.

---
+++ Array - Aula/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array___Aula
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] valores = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(string.Format("Valor {0}:", i));
+                valores[i] = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
